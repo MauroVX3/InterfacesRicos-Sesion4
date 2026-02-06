9ef61e0161a3a36a00b54c3eeaa9be69ba69f397
Mauro Varea Fernández <98806293+MauroVX3@users.noreply.github.com>
2026-02-06 18:59:48 +0100

    2. Formulario con select

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { useState, type ChangeEvent } from 'react';
 import './App.css'
 import EnrolmentForm from './components/EnrolmentForm'
 
 function App() {
+  const [program, setProgram] = useState("UG");
+  const [ugEnrolments, setUGEnrolments] = useState(0);
+  const [pgEnrolments, setPGEnrolments] = useState(0);
+
+  const handleChangeEnrolments = (updateEnrolments: number) => {
+    program == "UG" ? setUGEnrolments(updateEnrolments) : setPGEnrolments(updateEnrolments);
+  };
+  const handleChangeProgram = (event: ChangeEvent<HTMLLIElement>) => {
+    setProgram(event.target.value.toString());
+  };
+  const selectedEnrolments = (): number => {
+    return program == "UG" ? ugEnrolments : pgEnrolments;
+  }
+
   return (
-    <>
-      <EnrolmentForm />
-    </>
+    <div className="App">
+      <div className="programs">
+        <ul className="ulEnrol">
+          <li className="parentLabels" onChange={handleChangeProgram}>
+            <input type="radio" value="UG" name="programGroup" defaultChecked />
+            Grado
+            <input type="radio" className="radioSel" value="PG" name="programGroup" />
+            Postgrado
+          </li>
+          <li>Matriculaciones actuales: {ugEnrolments + pgEnrolments}</li>
+        </ul>
+      </div>
+      <EnrolmentForm chosenProgram={program} currentEnrolments={selectedEnrolments()} onChangeEnrolments={handleChangeEnrolments} />
+    </div>
   )
 }
 
--- src/components/EnrolmentForm.tsx
+++ src/components/EnrolmentForm.tsx
@@ -1,22 +1,33 @@
-import { useState, type FormEvent } from 'react';
+import { useRef, useState, type FormEvent } from 'react';
 import './EnrolmentForm.css'
 
-function EnrolmentForm() {
+interface EnrolmentFormProps { 
+    chosenProgram: string; 
+    currentEnrolments: number; 
+    onChangeEnrolments: (updateEnrolments: number) => void;  
+} 
+
+function EnrolmentForm(props: EnrolmentFormProps) {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [welcomeMessage, setWelcomeMessage] = useState("");
 
+    const nameInputRef = useRef<HTMLInputElement>(null);
+
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         setWelcomeMessage(`Bienvenido/a ${firstName} ${lastName}`);
+        props.onChangeEnrolments(props.currentEnrolments + 1);
+        event.currentTarget.reset(); // vaciamos el formulario 
+        nameInputRef.current?.focus(); // situamos el cursor en el campo fname 
         event.preventDefault();
     };
 
     return (
         <div>
             <form className="enrolForm" onSubmit={handleSubmit}>
-                <h1>Datos del estudiante</h1>
+                <h1>Datos del estudiante - {props.chosenProgram}</h1>
                 <label>Nombre:</label>
-                <input type="text" name="fname" onBlur={(event) => setFirstName(event.target.value)} />
+                <input type="text" name="fname" onBlur={(event) => setFirstName(event.target.value)} ref={nameInputRef} />
                 <br />
                 <label>Apellidos:</label>
                 <input type="text" name="lname" onBlur={(event) => setLastName(event.target.value)} />
